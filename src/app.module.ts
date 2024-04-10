ac29fe6f6486643514e44e85ada60185a0266414
jbbouchet <jb@studiomagnetique.fr>
2024-04-10 08:09:29 +0200

    fix: add SecurityModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CompanyModule } from '@strivee-api/company/infrastructure/company.module';
 import { FranceTravailModule } from '@strivee-api/france-travail/infrastructure/france-travail.module';
+import { SecurityModule } from '@strivee-api/security/infrastructure/security.module';
 import applicationConfig from './app.config';
 import { DatabaseModule } from './common/database/infrastructure/database.module';
 
@@ -13,6 +14,7 @@ import { DatabaseModule } from './common/database/infrastructure/database.module
       isGlobal: true,
     }),
     DatabaseModule,
+    SecurityModule,
     FranceTravailModule,
     CompanyModule,
   ],
